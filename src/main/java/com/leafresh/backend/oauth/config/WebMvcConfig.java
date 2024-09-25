56967e802e19cc8a9706e0f3d31dbfe301ffba5e
hwangjeonghan <166351290+hwangjeonghan@users.noreply.github.com>
2024-09-25 15:06:38 +0900

    Update WebMvcConfig.java

--- src/main/java/com/leafresh/backend/oauth/config/WebMvcConfig.java
+++ src/main/java/com/leafresh/backend/oauth/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 허용할 출처를 설정
+                .allowedOrigins("https://leafresh-frontend-alpha.vercel.app","https://leafresh-frontend-nine.vercel.app") // 허용할 출처를 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
