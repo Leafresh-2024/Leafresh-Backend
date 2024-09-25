beead519b704bdbbb7b128cefe2763899abfd0af
hwangjeonghan <166351290+hwangjeonghan@users.noreply.github.com>
2024-09-25 11:06:07 +0900

    debug

--- src/main/java/com/leafresh/backend/chat_with_spring/config/ChattingConfig.java
+++ src/main/java/com/leafresh/backend/chat_with_spring/config/ChattingConfig.java
@@ -21,7 +21,7 @@ public class ChattingConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws") // 연결을 위한 주소 웹 소켓의 연결 형식 ws://localhost:8080/ws
-                .setAllowedOrigins("*")
+                .setAllowedOrigins("https://leafresh-frontend-nine.vercel.app")
                 .withSockJS();
     }
 
